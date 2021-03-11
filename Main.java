bf049c8815ec3153df0f212cb6b7eb5a02b65270
Dan Lorenz Altaras <danlorenz06>
2021-03-11 23:02:45 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class assignment004
+{
+	public static void main(String[] args){
+		System.out.println("Dan Lorenz Altaras\n");
+		System.out.println("Miguel Andre Delos Angeles\n");
+		System.out.println("Miguel Paolo Salarda\n");
+	}
+}
